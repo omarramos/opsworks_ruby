46b20cc68557528a28db387317671402c7a6a685
Omar Ramos <omar@omarramos.com>
2017-08-31 11:11:20 -0400

    Use restart command after deploy instead of stop/start

--- libraries/drivers_appserver_base.rb
+++ libraries/drivers_appserver_base.rb
@@ -19,8 +19,7 @@ module Drivers
       end
 
       def after_deploy
-        manual_action(:stop)
-        manual_action(:start)
+        manual_action(:restart)
       end
       alias after_undeploy after_deploy
 
